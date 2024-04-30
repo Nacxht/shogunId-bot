19fcdc4bd83911aa1525d39cc72b3a3271f6c56c
Nacxht <zombiecrafter069@gmail.com>
2024-04-30 16:50:15 +0700

    create event handler for "autocomplete"

--- src/bot/events/on_autocomplete.ts
+++ src/bot/events/on_autocomplete.ts
@@ -1,9 +1,25 @@
 import { Client } from "discord.js";
+import { snakeToCamel } from "../utils/camel_caser.js";
+import { commands } from "../commands/_init.js";
+import { SlashCommand } from "../../../types/SlashCommand.js";
+import { logger } from "../utils/logger.js";
 
 export async function onAutocomplete(client: Client) {
-    client.on("interactionCreate", (interaction) => {
+    client.on("interactionCreate", async (interaction) => {
         if (!interaction.isAutocomplete()) return;
+        const commandName = await snakeToCamel(interaction.commandName);
+        const command = commands[commandName as keyof typeof commands] as SlashCommand;
 
-        // Code goes here
+        if (!command) return;
+        if (!(typeof command.autocomplete === "function")) return;
+
+        try {
+            await command.autocomplete(interaction);
+            return;
+        } catch (error: any) {
+            logger.error('An error happened in "Autocomplete Handler"');
+            logger.error(`${error.name} - ${error.message}`);
+            return;
+        }
     });
 }
