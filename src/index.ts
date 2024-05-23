317bbd753b266fa34996dccd4316f397cfb4bdd9
Nacxht <zombiecrafter069@gmail.com>
2024-05-23 20:25:10 +0700

    wrap code with main() function

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,22 @@ import { botConfig } from "./config/config.js";
 import { eventHandlerInit } from "./bot/events/_init.js";
 import { commandInit } from "./bot/commands/_init.js";
 
-// Create new Client instance
-const client: Client = new Client({
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.DirectMessages],
-});
+async function main() {
+    // Create new Client instance
+    const client: Client = new Client({
+        intents: [
+            GatewayIntentBits.Guilds,
+            GatewayIntentBits.GuildMessages,
+            GatewayIntentBits.DirectMessages,
+        ],
+    });
 
-// Initializer
-await commandInit(); // Deploy command
-await eventHandlerInit(client); // Event handler
+    // Initializer
+    await commandInit(); // Deploy command
+    await eventHandlerInit(client); // Event handler
 
-// Run bot
-client.login(botConfig.token);
+    // Run bot
+    client.login(botConfig.token);
+}
+
+main();
